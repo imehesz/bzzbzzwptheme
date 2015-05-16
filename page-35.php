43f7877a1be3e8ee5f70c7e9a475352b2478d8a0
Imre Mehesz <imehesz@gmail.com>
2015-05-16 05:16:41 +0400

    removed ads from the front-page for now, increased the random comics

--- page-35.php
+++ page-35.php
@@ -17,7 +17,7 @@
   $randArgs = array(
     'post_type'  => "comics",
     "orderby" => "rand",
-    "posts_per_page" => "4"
+    "posts_per_page" => "6"
   );
 ?>
 
@@ -28,7 +28,9 @@
     <?php get_sidebar( 'left' ); ?>
 
     <div class="col-md-<?php devdmbootstrap3_main_content_width(); ?> dmbs-main">
-      <p></p>
+      <p>
+        <h1>Featured Comics</h1>
+      </p>
       <div id="buzzCarousel" class="carousel slide" data-ride="carousel">
 
         <?php $the_query = new WP_Query( $args ); if ( $the_query->have_posts() ) : ?>
@@ -58,36 +60,42 @@
    
         <?php endif; wp_reset_postdata(); ?>
       </div>
-      <p></p>
-      <div class="row text_center">
-        <div class="col-md-6">
-        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <!-- bizzbuzz-read-top -->
-        <ins class="adsbygoogle"
-        style="display:block"
-        data-ad-client="ca-pub-1319358860215477"
-        data-ad-slot="6070069179"
-        data-ad-format="auto"></ins>
-        <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
-        </div>
-        <div class="col-md-6">
-        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <!-- bizzbuzz-read-top -->
-        <ins class="adsbygoogle"
-        style="display:block"
-        data-ad-client="ca-pub-1319358860215477"
-        data-ad-slot="6070069179"
-        data-ad-format="auto"></ins>
-        <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
-        </div>
+      <?php /*
+      <div id="buzzCarousel2" class="carousel slide" data-ride="carousel">
 
+        <?php $the_query2 = new WP_Query( $randArgs ); if ( $the_query2->have_posts() ) : ?>
+                <div class="carousel-inner">
+              <?php $cnt=0; while ( $the_query2->have_posts() ): ?>
+              <?php if($cnt == 0 || $cnt % 3 == 0) : ?>
+                  <div class="item <?php echo $cnt == 0 ? "active" : "" ?>">
+              <?php endif; ?>
+              <?php $the_query->the_post(); $cp = new ComicParser($post); ?>
+                <div class="featured-wrapper">
+                  <a class="featured-link" href="<?php echo get_permalink($cp->getId()); ?>"><img class="img-responsive" src="<?php echo $cp->getCover(); ?>" alt="<?php echo $cp->getTitle(); ?>"></a>
+                  <div class="featured-info-wrapper">
+                    <h1><a class="featured-link" href="<?php echo get_permalink($cp->getId()); ?>"><?php echo $cp->getTitle(); ?></a></h1>
+                  </div>
+                </div>
+              <?php $cnt++; ?>
+
+              <?php if ($cnt ==0 || $cnt % 3 == 0 || $cnt == $the_query2->found_posts) : ?>
+                  </div>
+              <?php endif; ?>
+
+            <?php endwhile; ?>
+                </div>
+    
+            <a class="left carousel-control" onclick="return false;" href="#buzzCarousel2" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+            <a class="right carousel-control" onclick="return false;" href="#buzzCarousel2" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+   
+        <?php endif; wp_reset_postdata(); ?>
       </div>
-      <p><br /></p>
+*/?>
       <p></p>
+      <p><br /></p>
+      <p>
+        <h2>Random Comics</h2>
+      </p>
       <?php $the_query3 = new WP_Query( $randArgs ); if ( $the_query3->have_posts() ) : ?>
         <div class="row text-center random-four">
         <?php $cnt=0; while ( $the_query3->have_posts() ): ?>
